ea4b11b9684a7a16a3403d5a96a69040cf1ad17c
Swbastiann <jsebastib@gmail.com>
2024-11-15 22:37:57 -0500

    Add files via upload

---
+++ Api_Front/Api_Front/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Api_Front.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly string _connectionString = "Server=LAPTOP-LFA03L5H\\MSSQLSERVER01;Database=dbtest;User Id=sa;Password=12345678;TrustServerCertificate=true";
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Users user)
+
+        //un método de acción en un controlador de ASP.NET Core que recibe datos de una solicitud HTTP y devuelve un resultado de acción.
+        //Indica que el método espera un objeto user de tipo Users en el cuerpo de la solicitud HTTP. El atributo [FromBody]
+        //le dice a ASP.NET Core que los datos deben ser deserializados desde el cuerpo de la solicitud en un objeto Users
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid user data.");
+            }
+            using (var connection = new SqlConnection(_connectionString))
+            {   
+
+                var sql = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password"; //@ define un parametro
+                var result = connection.QuerySingleOrDefault<Users>(sql, new { user.Username, user.Password });
+
+                if (result != null)
+                {
+
+                    return Ok("Login successful.");
+                }
+                else
+                {
+                    return Unauthorized("Invalid credentials.");
+                }
+            }
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] Users user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid user data.");
+            }
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "INSERT INTO Users (Username, Password) VALUES (@Username, @Password)";
+                var rowsAffected = connection.Execute(sql, new { user.Username, user.Password });
+
+                if (rowsAffected > 0)
+                {
+                    return Ok("User registered successfully.");
+                }
+                else
+                {
+                    return StatusCode(500, "An error occurred while registering the user.");
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Api_Front/Api_Front/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Api_Front
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
