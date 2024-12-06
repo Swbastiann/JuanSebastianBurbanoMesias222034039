d11c65afdf0aaba8aaedc3bebb5d1755ffb4b662
Brandon Moya <moyabec@gmail.com>
2024-12-06 18:11:00 -0500

    Front Mejorado

--- Biblioteca_front/src/app/app.config.ts
+++ Biblioteca_front/src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideHttpClient(), provideRouter(routes)]
