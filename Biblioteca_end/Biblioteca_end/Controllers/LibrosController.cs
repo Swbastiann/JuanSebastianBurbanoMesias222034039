06e793047a0cb96bd068c0900cd89e2b2488f3f6
Sebastian <jsebastib@gmail.com>
2024-12-06 11:16:14 -0500

    Avance Backend

---
+++ Biblioteca_end/Biblioteca_end/Controllers/LibrosController.cs
@@ -0,0 +1,121 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Biblioteca_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly string _connectionString = "Server=LAPTOP-LFA03L5H\\MSSQLSERVER01;Database=Biblioteca;User Id=sa;Password=12345678;TrustServerCertificate=true";
+
+        //Registrar
+        [HttpPost("Register")]
+        public IActionResult Register([FromBody] Libros libro)
+        {
+            if (libro == null)
+            {
+                return BadRequest(new { status = "error", message = "Invalid data." });
+            }
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                // Verifica si el libro ya está registrado
+                var sqlCheckBook = "SELECT COUNT(*) FROM Libros WHERE isbn = @Isbn";
+                var bookExists = connection.ExecuteScalar<int>(sqlCheckBook, new { libro.Isbn }) > 0;
+
+                if (bookExists)
+                {
+                    return BadRequest(new { status = "error", message = "Book is already registered." });
+                }
+
+                // Si el libro no existe, se inserta en la base de datos
+                var sql = "INSERT INTO Libros (isbn, titulo, autor, editorial, genero, cantidad, año_publicacion) VALUES (@Isbn, @Titulo, @Autor, @Editorial, @Genero, @Cantidad, @Año_publicacion)";
+                var result = connection.Execute(sql, new { libro.Isbn, libro.Titulo, libro.Autor, libro.Editorial, libro.Genero, libro.Cantidad, libro.Año_publicacion });
+                if (result > 0)
+                {
+                    return Ok(new { status = "success", message = "Book registered successfully." });
+                }
+                else
+                {
+                    return StatusCode(500, new { status = "error", message = "Error registering book." });
+                }
+            }
+        }
+
+        //Eliminar
+        [HttpDelete("delete/{isbn}")]
+        public IActionResult Delete(string isbn)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "DELETE FROM Libros WHERE isbn = @Isbn";
+                var rowsAffected = connection.Execute(sql, new { Isbn = isbn });
+
+                if (rowsAffected > 0)
+                {
+                    return Ok("Book deleted successfully.");
+                }
+                else
+                {
+                    return NotFound("Book not found.");
+                }
+            }
+        }
+
+        //Obtener todos los libros
+        [HttpGet("getBooks")]
+        public IActionResult GetBooks()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    //consulta SQL
+                    var sql = "SELECT * FROM Libros ORDER BY genero";
+                    var libros = connection.Query<Libros>(sql).ToList();
+
+                    if (libros == null || libros.Count == 0)
+                    {
+                        return NotFound("No books found.");
+                    }
+
+                    return Ok(libros);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        //Obtener informacion de un libro por ISBN
+        [HttpGet("getByIsbn/{isbn}")]
+        public IActionResult GetByIsbn(string isbn)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    // Consulta SQL para obtener el libro
+                    var sql = "SELECT * FROM Libros WHERE isbn = @Isbn";
+                    var libro = connection.QuerySingleOrDefault<Libros>(sql, new { Isbn = isbn });
+
+                    if (libro == null)
+                    {
+                        return NotFound($"Book with ISBN {isbn} not found.");
+                    }
+                    return Ok(libro);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Si hay un error, devolvemos un mensaje de error
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Biblioteca_end/Biblioteca_end/Libros.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblioteca_end
+{
+    public class Libros
+    {
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; } 
+        public string Editorial { get; set; }
+        public string Genero { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime Año_publicacion { get; set; }
+
+    }
+}
