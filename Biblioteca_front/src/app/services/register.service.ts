be414570e1b1574e7859d5e9751558f1a27cbb83
Sebastian <jsebastib@gmail.com>
2024-12-07 08:37:59 -0500

    Avance Backend

---
+++ Biblioteca_front/src/app/services/register.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  apiUrl = 'https://localhost:7011/api/libros/Register';
+
+  constructor(private http: HttpClient) {}
+
+  registerBook(book: any): Observable<any> {
+    return this.http.post(this.apiUrl, book);
+  }
+  
+}
